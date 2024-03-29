87df4276a4bdc1b95b9910da33751d6eaff1f0f5
SONY <bantempriyanka@gmail.com>
2024-03-29 19:33:02 +0530

    Added one package in this Package and added Testscripts in allAnnotations

---
+++ QWERTY/src/AgainAdded/Github.java
@@ -0,0 +1,12 @@
+package AgainAdded;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Github {
+@Test
+public void githubb()
+{
+	Reporter.log("test", true);
+}
+}
---
+++ QWERTY/src/allAnnotations/Adding_one.java
@@ -0,0 +1,12 @@
+package allAnnotations;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Adding_one {
+@Test
+public void Testt()
+{
+	Reporter.log("test");
+}
+}
